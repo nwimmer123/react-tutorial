b7e315cf2e26013b5ca0d1e8aec2109aa826e867
Noah Wimmer <nwimmer123@yahoo.com>
2016-09-26 12:52:16 -0700

    comment form calling

--- src/commentBox.js
+++ src/commentBox.js
@@ -1,12 +1,36 @@
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, I'm a CommentList
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function () {
+    return (
+      <div className="CommentForm">
+        I am a comment Form
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, I'm a box
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>  
     );
   }
 });
+
+
 
 ReactDOM.render(
   <CommentBox />,
